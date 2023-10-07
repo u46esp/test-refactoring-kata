b9b707f9d42dfe7fc0b20dd3f229b95da180c3c2
u46esp <u46esp@gmail.com>
2023-10-07 22:37:43 +0000

    Populate initial data

--- src/index.js
+++ src/index.js
@@ -8,8 +8,21 @@ const root = createRoot(rootElement);
 
 const seatData = {};
 
+const seatPricing = [
+  { zone: "furthest", regularPrice: 2500, premiumMemberPrice: 2125 },
+  { zone: "mid", regularPrice: 3500, premiumMemberPrice: 2975 },
+  { zone: "side", regularPrice: 4000, premiumMemberPrice: 3400 },
+  { zone: "standing", regularPrice: 3000, premiumMemberPrice: 2550 },
+  { zone: "frontmost", regularPrice: 6000, premiumMemberPrice: 5100 },
+  { zone: "viphitouch", regularPrice: 2500, premiumMemberPrice: 6375 },
+];
+
 root.render(
   <StrictMode>
-    <App seatData={seatData} isCustomerPremiumMember={true} />
-  </StrictMode>
+    <App
+      seatData={seatData}
+      seatPricing={seatPricing}
+      isCustomerPremiumMember={true}
+    />
+  </StrictMode>,
 );
